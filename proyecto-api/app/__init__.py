1429baf59be2d1335e6f112a3c3677a71c6f4392
AgustinR-Fing <203549796+AgustinR-Fing@users.noreply.github.com>
2026-08-02 00:00:04 -0300

    refactor : change order inside create_app()

--- proyecto-api/app/__init__.py
+++ proyecto-api/app/__init__.py
@@ -4,16 +4,18 @@ from dotenv import load_dotenv
 from app.database import db
 
 def create_app(): # Factory
-    app = Flask(__name__) # Se crea la instancia
 
-    @app.route("/ping") # Registra la funcion ping() como endpoint de la URL /ping (no modifica la funcion)
-    def ping():
-        return {"status": "ok"}
+    app = Flask(__name__) # Se crea la instancia
 
     # Configurar el traductor de SQLAlchemy, que instanciamos en database.py
     load_dotenv()
     database_url = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     db.init_app(app) # Le indico al traductor donde tiene que trabajar (cual base de datos)
+
+    # Registrar rutas
+    @app.route("/ping") # Registra la funcion ping() como endpoint de la URL /ping (no modifica la funcion)
+    def ping():
+        return {"status": "ok"}
 
     return app
