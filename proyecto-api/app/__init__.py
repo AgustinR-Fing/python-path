7c19bbf3784dae478f6e3865d2661efc5ed0c2ef
AgustinR-Fing <203549796+AgustinR-Fing@users.noreply.github.com>
2026-08-01 23:54:16 -0300

    feat: configure database connection from environment

--- proyecto-api/app/__init__.py
+++ proyecto-api/app/__init__.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
-
+from dotenv import load_dotenv
+from app.database import db
 
 def create_app(): # Factory
     app = Flask(__name__) # Se crea la instancia
@@ -7,5 +9,11 @@ def create_app(): # Factory
     @app.route("/ping") # Registra la funcion ping() como endpoint de la URL /ping (no modifica la funcion)
     def ping():
         return {"status": "ok"}
+
+    # Configurar el traductor de SQLAlchemy, que instanciamos en database.py
+    load_dotenv()
+    database_url = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    db.init_app(app) # Le indico al traductor donde tiene que trabajar (cual base de datos)
 
     return app
---
+++ proyecto-api/app/database.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy
+#    └─ módulo               └─ clase
+db = SQLAlchemy() # Creo el objeto 'traductor' python <-> sql (todavia no le dijimos que base de datos)
